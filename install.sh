954c66832a364689bd49dac85e569f29af077a6d
Julius <Semicolon255@users.noreply.github.com>
2021-07-09 15:47:29 +0200

    add echo messages

--- install.sh
+++ install.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
 
+echo "Downloadig repo"
 git clone https://github.com/Semicolon255/dotfiles.git ~/.dotfiles
 cd ~/.dotfiles
 git pull
 
+echo "Installing .zshrc"
 rm ~/.zshrc
 ln -s ~/.dotfiles/.zshrc ~/.zshrc
 
-rm ~/.gitconfig
-ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
-
+echo "Installing .p10k.zsh"
 rm ~/.p10k.zsh
 ln -s ~/.dotfiles/.p10k.zsh ~/.p10k.zsh
+
+echo "Installing .gitconfig"
+rm ~/.gitconfig
+ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
